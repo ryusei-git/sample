9f1d89d98c38e46bff321db4d8f3d2bd205d8415
ryusei <private.ryusei@gmail.com>
2021-11-21 18:29:22 +0900

    SHA-512処理作成

--- java/src/App.java
+++ java/src/App.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class App {
     private static Scanner scanner = new Scanner(System.in);
     private static boolean boolean_tmp;
@@ -17,6 +17,7 @@ public class App {
         if(parse_data[0]=="true"){
             System.out.println("**********処理完了**********");
         }
+        encryptString();
     }
     public static String[] String_parse(String Word){
         // 正規表現（判定基準）
@@ -48,6 +49,17 @@ public class App {
         return result;
     }
     public static String encryptString(){
+        // SHA-512（SHA-2）
+        MessageDigest sha512;
+        // 暗号化される文字
+        String enc_String = "これはテストです";
+        try {
+            sha512 = MessageDigest.getInstance("SHA-512");
+            byte[] sha512_result = sha512.digest(enc_String.getBytes());
+        System.out.println("SHA-512：" + String.format("%040x", new BigInteger(1, sha512_result)));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
